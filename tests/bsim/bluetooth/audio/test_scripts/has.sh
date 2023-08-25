7b76d401d525dfbe300971a9ef6bdb039fff2e40
Mariusz Skamra <mariusz.skamra@codecoup.pl>
2023-08-25 16:56:40 +0200

    tests: Bluetooth: has: Add test Preset Changed Offline Behavior
    Verify that a HAS Server IUT sends changed characteristic notifications
    or indications when the Lower Tester reconnects.
    
    Signed-off-by: Mariusz Skamra <mariusz.skamra@codecoup.pl>

--- tests/bsim/bluetooth/audio/test_scripts/has.sh
+++ tests/bsim/bluetooth/audio/test_scripts/has.sh
@@ -6,7 +6,6 @@
 
 source ${ZEPHYR_BASE}/tests/bsim/sh_common.source
 
-SIMULATION_ID="has"
 VERBOSITY_LEVEL=2
 EXECUTE_TIMEOUT=20
 
@@ -14,11 +13,28 @@ cd ${BSIM_OUT_PATH}/bin
 
 printf "\n\n======== Running HAS main (API) test =========\n\n"
 
+SIMULATION_ID="has"
+
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=has -rs=24
 
 Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
   -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=has_client -rs=46
+
+Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} \
+  -D=2 -sim_length=60e6 $@
+
+wait_for_background_jobs
+
+printf "\n\n Running Preset Changed Offline Behavior test \n\n"
+
+SIMULATION_ID="has_offline_behavior"
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=0 -testid=has_offline_behavior -rs=24
+
+Execute ./bs_${BOARD}_tests_bsim_bluetooth_audio_prj_conf \
+  -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} -d=1 -testid=has_client_gatt -rs=46
 
 Execute ./bs_2G4_phy_v1 -v=${VERBOSITY_LEVEL} -s=${SIMULATION_ID} \
   -D=2 -sim_length=60e6 $@
